c298335b19672aa274a31aa8d340a19b66fc84e4
shahfaisal332 <shahff0332@gmail.com>
2026-06-13 12:34:30 +0500

    Implement basic number storage contract functions

--- level-1-basic-contracts/simpleNumberStorage.sol
+++ level-1-basic-contracts/simpleNumberStorage.sol
@@ -1,1 +1,23 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.0;
+
+contract numberStorage{
+    uint public number;
+
+    function setNumber(uint _number) public{
+        number = _number;
+    }
+
+    function getNumber() public view returns(uint){ 
+        return number;
+    }
+
+    function increaseNumber() public{
+        number += 1;
+    }
+
+    function decreaseNumber() public{
+        number -= 1;
+    }
+}
