85552a5b16cd04829cdad7f0d6c71665e548ac77
shahfaisal332 <shahff0332@gmail.com>
2026-06-13 12:32:03 +0500

    Add HelloSolidity contract with set and get functions

--- level-1-basic-contracts/HelloSolidity.sol
+++ level-1-basic-contracts/HelloSolidity.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.7;
+
+contract helloSoliditiy{
+    string public massage;
+
+    function setMassage(string memory _massage) public{
+        massage = _massage;
+    }
+
+    function getMassage()public view returns (string memory){
+        return massage;
+    }
+
+}
