fa5959e5d6e0127168b46b2e82a8779de72a1c00
shahfaisal332 <shahff0332@gmail.com>
2026-06-13 18:32:10 +0500

    Update onlyOwner.sol

--- level-1-basic-contracts/onlyOwner.sol
+++ level-1-basic-contracts/onlyOwner.sol
@@ -7,14 +7,14 @@ contract onlyowner{
     string public secretMassage;
 
     constructor(){
-        owner = msg.sender;
+        owner = msg.sender;   //msg.sender means deployer's address
     }
-
+   //Only Owner can call this
     function setmassage(string memory _massage) public{
         require(msg.sender == owner, "only owner can massage");
         secretMassage = _massage;
     }
-
+ // Everyone can call this
     function getmassage()public view returns(string memory){
         return secretMassage;
     }
