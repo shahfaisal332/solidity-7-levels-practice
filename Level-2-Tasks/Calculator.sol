d17d37610ff9d908302bdebabe049f41ba3c84d4
shahfaisal332 <shahff0332@gmail.com>
2026-06-14 20:54:29 +0500

    Update Calculator.sol

--- Level-2-Tasks/Calculator.sol
+++ Level-2-Tasks/Calculator.sol
@@ -1,1 +1,25 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity  ^0.8.7;
+
+contract Calculator{
+
+    function add(uint _a,uint _b)public pure returns(uint){
+        return _a + _b;
+    }
+    //subtract should not allow negative result
+    function subtract(uint _a,uint _b) public pure returns(uint){
+        require(_a >= _b,"Answer would be negative");
+        return _a - _b;
+    }
+    
+    function multiply(uint _a, uint _b)public pure returns(uint){
+        return _a * _b;
+    }
+    //divide should not allow division by zero.
+    function divide(uint _a, uint _b) public pure returns(uint){
+        require(_b != 0, "Cannot divid by 0");
+        return _a/_b;
+    }
+
+}
