e2af9e159c8486fa4f882008b09228a09d1d087a
shahfaisal332 <shahff0332@gmail.com>
2026-06-14 13:52:09 +0500

    Implement SimpleWallet contract with basic functions

--- level-1-basic-contracts/simpleWallet.sol
+++ level-1-basic-contracts/simpleWallet.sol
@@ -1,1 +1,31 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.7;
+
+contract SimpleWallet{
+
+    address payable public owner;
+
+    constructor(){
+        owner =payable (msg.sender);
+    }
+
+    modifier Onlyowner(){
+        require(msg.sender == owner, "Only owner can withdraw");
+        _;
+    }
+
+    function deposite()payable public{
+        //Anyone can deposite
+    }
+
+    function getBalance() public view returns(uint){
+        return address(this).balance;
+    }
+
+    function withdraw(uint _amount) public Onlyowner{
+        require(_amount <= address(this).balance, "Insufficient Balance");
+
+        payable(owner).transfer(_amount);
+    } 
+}
