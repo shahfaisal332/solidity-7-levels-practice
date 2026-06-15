16f3520b3e3ff95d217c491c7c221a84db035070
shahfaisal332 <shahff0332@gmail.com>
2026-06-15 09:59:34 +0500

    Implement OwnerControlledCounter contract

--- Level-2-Tasks/OwnerControlledCounter.sol
+++ Level-2-Tasks/OwnerControlledCounter.sol
@@ -1,1 +1,48 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity  ^0.8.7;
+
+contract OwnerControlledCounter{
+    
+    address public owner;
+    uint public count;
+
+    event TransferOwnerShip(address indexed oldOwner, address indexed newOwner);
+    event Increase(uint newCount);
+    event Decrease(uint newCount);
+    event Reset();
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "not owner");
+        _;
+    }
+
+
+    function increase() public onlyOwner{
+        count += 1;
+        emit Increase(count);
+
+    }
+
+    function decrease() public onlyOwner{
+        count -= 1;
+        emit Decrease(count);
+    }
+
+    function reset() public onlyOwner{
+        count = 0;
+        emit Reset();
+    }
+
+    function transferOwnership(address _newOwner) public onlyOwner{
+        require(_newOwner != address(0), "New owner is zero address");
+        address oldOwner = owner;
+        owner = _newOwner;
+        emit TransferOwnerShip(oldOwner, _newOwner);
+    }
+
+}
